2901652c86be160bd202adee17a34559ba321d76
majesticrattoon <140112849+majesticrattoon@users.noreply.github.com>
2023-07-26 14:21:52 +0200

    Add files via upload

--- InstallScript.sh
+++ InstallScript.sh
@@ -3,13 +3,17 @@ echo "Install script activated"
 
 sleep 1
 
-sudo pacman -S amd-ucode mesa noto-fonts lib32-mesa xf86-video-amdgpu amdvlk xorg-server xorg-apps i3-wm i3blocks i3status xss-lock i3lock nano neovim jdk-openjdk thunar firewalld firefox base-devel alacritty neofetch xdg-user-dirs sysstat pulsemixer rofi picom lxappearance feh code
+cd
+
+sudo pacman -S amd-ucode mesa neofetch noto-fonts p7zip lib32-mesa xf86-video-amdgpu amdvlk xorg-server xorg-apps i3-wm i3blocks i3status xss-lock i3lock nano ly neovim jdk-openjdk thunar firewalld firefox base-devel alacritty neofetch xdg-user-dirs sysstat pulsemixer rofi picom lxappearance feh code reflector
 
 xdg-user-dirs-update
 
 sudo pacman -Rns xdg-user-dirs 
 
 sudo systemctl enable firewalld
+
+sudo systemctl enable ly
 
 mkdir ~/.config/i3
 
@@ -40,5 +44,21 @@ mv /home/lukas/Arch/i3blocks/config /home/lukas/.config/i3blocks/
 mv /home/lukas/Arch/picom/picom.conf /home/lukas/.config/picom/
 
 mv /home/lukas/Arch/alacritty/alacritty.yml /home/lukas/.config/alacritty/
+
+cd
+
+git clone https://aur.archlinux.org/yay-bin.git
+
+cd yay-bin/
+
+makepkg -si
+
+cd
+
+rm -fr yay-bin/
+
+yay
+
+yay -S ttf-jetbrains-mono-nerd rofi-power-menu
 
 echo "Script finished"
